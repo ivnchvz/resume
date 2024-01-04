2793565efa3c00d8af96ac05ba18aa183333ac3d
ivnchvz <chavezromeroivan@gmail.com>
2024-01-04 02:44:23 -0800

    Changed test

--- resume_backend/test_function_app.py
+++ resume_backend/test_function_app.py
@@ -5,12 +5,13 @@ from azure.functions import HttpRequest
 def test_http_trigger():
     # Setup
     req = HttpRequest(method='GET', url='http://localhost:7071/api/HttpTrigger', params={}, body='')
-
+    
     # Exercise
     resp = http_trigger(req)
     print(resp)  # Add this line
-
+    
     # Verify
-    assert resp.status_code == 200  # check if status code is 200
-
-    # Cleanup - none necessary for this simple test
+    if resp is not None:
+        assert resp.status_code == 200  # check if status code is 200
+    else:
+        assert False, "http_trigger returned None"
